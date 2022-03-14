8b0ec3ce6793e63337043bb97c753b30f13b1595
Andrew Pollak <79533175+apollak1@users.noreply.github.com>
2022-03-14 15:58:02 -0500

    Add files via upload

---
+++ Magic Eight Ball Arrays Mini-Test/main.swift
@@ -0,0 +1,26 @@
+//
+//  main.swift
+//  Magic Eight Ball Arrays Mini-Test
+//
+//  Created by Pollak, Andrew on 3/14/22.
+//
+
+import Foundation
+
+//MAGIC EIGHT BALL ARRAYS TEST
+
+//To do this test you may look up anything in your work, Canvas, labs, or the posters on the walls...hint hint. 
+
+//Create a new array "answers" and populate the array with the magicEightBall phrases. Do not declare a type.
+//Use all these magicEightBall phrases to populate the "answers" array: It is certain, It is decidedly so, Without doubt, You may rely on it, As I see it, yes, Most likely, Outlook good, Yes, Signs point to yes, Reply hazy, try again later, Ask again later, Better not tell you now, Cannot predict now, Concentrate and ask again, Don't count on it, My sources say no, Outlook not so good, Very doubtful
+//
+//Open your Functions Lab Part 1
+//Find the Magic Eight Ball solutions code 1.1 you did and copy it.
+//Paste it below at the bottom.
+//
+//Delete all the If/Else-if/Else statements in the function.
+//Change the Int.random last parameter from 19 to the new array name, counting it and subtracting one. Remember, 0-19 IS 20 items.
+//Inside the function print the answers array INDEXING randomNumber.
+//Call the function 10 times on 10 lines to test it.
+//When complete, push up to GitHub with the note saying “Magic8Ball completed”.
+
